a7721f9a3403e3d9b60d4fd7fbad9562d99621da
Camilo Rodríguez <aeroxmotion@hotmail.com>
2018-12-15 22:06:47 -0500

    fix: route param optional token

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 const PARAM_REGEX = /:(?<param>\w+)(?<optional>\?)?/g
 
 export function compileRouteRegex (path) {
-  const regex = path.replace(PARAM_REGEX, (_, param, optional) => {
+  const regex = path.replace(PARAM_REGEX, (_, param, optional = '') => {
     return `(?:<${param}>[^/]+)${optional}`
   })
 
