60c5d32cb064feb7115ff6904b00c93611783551
Camilo Rodríguez <aeroxmotion@hotmail.com>
2018-12-15 22:05:17 -0500

    feat: allow route params

--- src/GalaxyRoute.js
+++ src/GalaxyRoute.js
@@ -1,18 +1,46 @@
+import { compileRouteRegex } from './utils.js'
+
 export default class GalaxyRoute {
+
+  /**
+   * Path matcher
+   *
+   * @type {RegExp}
+   */
+  _matcher = null
+
+  /**
+   * Route params
+   *
+   * @type {Object.<string>}
+   */
+  params = {}
+
   constructor (route) {
     this._def = route
 
     this.path = route.path
     this.element = route.element
 
-    this._register()
+    this._init()
   }
 
   match (path) {
-    return this.path === path
+    const matched = this._matcher.exec(path)
+
+    if (matched) {
+      this.params = { ...matched.groups }
+    }
+
+    return !!matched
   }
 
-  _register () {
+  _init () {
+
+    // Compile matcher
+    this._matcher = compileRouteRegex(this.path)
+
+    // Register element
     window.customElements.define(this.element.is, this.element)
   }
 }
---
+++ src/utils.js
@@ -0,0 +1,9 @@
+const PARAM_REGEX = /:(?<param>\w+)(?<optional>\?)?/g
+
+export function compileRouteRegex (path) {
+  const regex = path.replace(PARAM_REGEX, (_, param, optional) => {
+    return `(?:<${param}>[^/]+)${optional}`
+  })
+
+  return new RegExp(`^${regex}$`)
+}
